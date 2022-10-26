d92e8813c4c15c2ab299e9818d2477e7cb3618b5
Marianne Brisson <84756962+brim1503@users.noreply.github.com>
2022-10-26 00:11:53 -0400

    commit test project

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+    //[SerializeField] private messageInput;
+    // Start is called before the first frame update
+    private bool displaySymbolError = false;
+    public GameObject errorText;
+    public InputField inputField;
+    public Button submitButton;
+    
+
+    //private gameObject submitButton;
+    void Start()
+    {
+        submitButton.onClick.AddListener(OnSubmit);
+    }
+
+    void Update()
+    {
+
+        errorText.SetActive(displaySymbolError);
+    }
+
+    void OnSubmit()
+    {
+        string input = inputField.text;
+        if (Regex.IsMatch(input, "^[a-zA-Z0-9]*$"))
+        {
+            displaySymbolError = false;
+        }
+        else
+        {
+            displaySymbolError = true;
+        }
+        errorText.SetActive(displaySymbolError);
+        Debug.Log("inputfield text:" + input);
+    }
+}
